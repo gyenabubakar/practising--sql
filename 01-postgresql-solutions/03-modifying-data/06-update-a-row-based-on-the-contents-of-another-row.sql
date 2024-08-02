78d3edf46b118c4bbc370165373e5d0e18444544
Gyen Abubakar Sadick <indehyde@gmail.com>
2024-08-02 16:14:38 +0000

    sql: solved C03:Q06

---
+++ 01-postgresql-solutions/03-modifying-data/06-update-a-row-based-on-the-contents-of-another-row.sql
@@ -0,0 +1,11 @@
+WITH first_prices_plus_10_percent (member, guest) AS (
+    SELECT
+        (membercost * 0.1) as member,
+        (guestcost * 0.1) as guest
+    FROM cd.facilities
+    WHERE facid = 0
+)
+UPDATE cd.facilities
+SET membercost = membercost + (SELECT member FROM first_prices_plus_10_percent),
+    guestcost = guestcost + (SELECT guest FROM first_prices_plus_10_percent)
+WHERE facid = 1;
